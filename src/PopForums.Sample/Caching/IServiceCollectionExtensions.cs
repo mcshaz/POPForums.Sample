2d17946ef2aad757e2e9b00a61e5dce7eba65d4d
Brent McSharry <2456704+mcshaz@users.noreply.github.com>
2020-03-11 16:42:35 +1300

    cache busting

---
+++ src/PopForums.Sample/Caching/IServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace PopForums.Sample.Caching
+{
+    public static class IServiceCollectionExtensions
+    {
+
+        public static void AddUrlHelper(this IServiceCollection services)
+        {
+            services.TryAddSingleton<IActionContextAccessor, ActionContextAccessor>();
+            services.TryAddSingleton<IUrlHelperFactory, UrlHelperFactory>();
+
+            services.TryAddScoped<IUrlHelper>(
+                factory => factory.GetRequiredService<IUrlHelperFactory>()
+                    .GetUrlHelper(factory.GetRequiredService<IActionContextAccessor>().ActionContext)
+            );
+        }
+
+    }
+}
