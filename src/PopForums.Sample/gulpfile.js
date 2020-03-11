65ada153feb67a23367b8357d972793c227a45eb
Brent McSharry <2456704+mcshaz@users.noreply.github.com>
2020-03-11 16:41:47 +1300

    towards working front page

--- src/PopForums.Sample/gulpfile.js
+++ src/PopForums.Sample/gulpfile.js
@@ -1,22 +1,33 @@
-﻿/// <binding AfterBuild="default" />
+﻿/// <binding BeforeBuild='merge-js, transfer' />
 
-var gulp = require("gulp"),
-	merge = require("merge-stream");
+const gulp = require("gulp");
+const merge = require("merge-stream");
+const concat = require('gulp-concat');
+const debug = require('gulp-debug');
+// const minify = require('gulp-minify');
+// const cleanCss = require('gulp-clean-css');
 
-var nodeRoot = "./node_modules/";
-var targetPath = "./wwwroot/lib/";
+const nodeRoot = "./node_modules/";
+const targetPath = "./wwwroot/lib/";
 
-gulp.task("default", function () {
+gulp.task("transfer", function () {
 	var streams = [
-		gulp.src(nodeRoot + "bootstrap/dist/**/*").pipe(gulp.dest(targetPath + "/bootstrap/dist")),
-		gulp.src(nodeRoot + "popper.js/dist/umd/popper.min.js").pipe(gulp.dest(targetPath + "/popper.js/dist")),
-		gulp.src(nodeRoot + "jquery/dist/**/*").pipe(gulp.dest(targetPath + "/jquery/dist")),
+		gulp.src(nodeRoot + "bootstrap/dist/**/*.min.css").pipe(gulp.dest(targetPath + "/bootstrap/dist")),
 		gulp.src(nodeRoot + "@microsoft/signalr/dist/browser/**/*").pipe(gulp.dest(targetPath + "/signalr/dist")),
 		gulp.src(nodeRoot + "tinymce/**/*").pipe(gulp.dest(targetPath + "/tinymce")),
 		gulp.src(nodeRoot + "vue/dist/**/*").pipe(gulp.dest(targetPath + "/vue/dist")),
 		gulp.src(nodeRoot + "vue-router/dist/**/*").pipe(gulp.dest(targetPath + "/vue-router/dist")),
 		gulp.src(nodeRoot + "axios/dist/**/*").pipe(gulp.dest(targetPath + "/axios/dist")),
-		gulp.src(nodeRoot + "@popworldmedia/popforums/**/*").pipe(gulp.dest(targetPath + "/PopForums"))
-	];
+		gulp.src(nodeRoot + "@popworldmedia/popforums/**/*").pipe(gulp.dest(targetPath + "/PopForums"))];
 	return merge(streams);
 });
+
+gulp.task("merge-js", function () {
+	// the bundle package includes popper: "popper.js/dist/umd/popper.min.js"
+	const jsstreams = ["jquery/dist/**/jquery.min.js", "bootstrap/dist/**/bootstrap.bundle.min.js"].map((s) => nodeRoot + s);
+	return gulp.src(jsstreams)
+		.pipe(debug())
+		.pipe(concat('bundle.min.js'))
+		.pipe(gulp.dest(targetPath));
+});
+
